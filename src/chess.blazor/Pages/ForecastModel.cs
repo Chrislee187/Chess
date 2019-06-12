13023f0a5601a9ccbbd1cc158e98097de3ac2d2b
Chris Lee <chrislee187@gmail.com>
2019-06-12 22:13:32 +0100

    Add chess.blazor, basics of the SPA

---
+++ src/chess.blazor/Pages/ForecastModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace chess.blazor.Pages
+{
+    public class ForecastModel
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
