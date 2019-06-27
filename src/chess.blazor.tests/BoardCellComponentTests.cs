f1cafc4c1486bd457fa0da846b27b94bbbc8cd25
Chris Lee <chrislee187@gmail.com>
2019-06-27 15:27:12 +0100

    Add BoardCellComponentTests

---
+++ src/chess.blazor.tests/BoardCellComponentTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using chess.blazor.Shared.Chess;
+using chess.webapi.client.csharp;
+using Microsoft.AspNetCore.Components;
+using Moq;
+using NUnit.Framework;
+
+namespace chess.blazor.tests
+{
+    [TestFixture]
+    public class BoardCellComponentTests
+    {
+        private BoardCellComponent _component;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _component = new BoardCellComponent();
+        }
+
+        private const string AnyMove = "a1a2";
+
+        [TestCase(' ')]
+        [TestCase('.')]
+        public void IsEmptySquare_returns_true(char piece)
+        {
+            _component.Piece = piece;
+
+            Assert.True(_component.IsEmptySquare);
+        }
+
+        [Test]
+        public void OnClick_uses_OnPieceSelected_callback()
+        {
+            // TODO: Find how to test this once I get my internet back:(
+//            var handleEvent = new Mock<IHandleEvent>();
+//            var x = new EventCallback<PieceSelectedEventArgs>();
+//
+//            _component.OnPieceSelected = x;
+//
+//            _component.OnClick();
+            Assert.Inconclusive();
+        }
+    }
+}
